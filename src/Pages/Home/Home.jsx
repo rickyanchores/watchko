adb66aeaf0b1721ae3248766df24df4efb74d8c3
RickyAnchores <ricky23anchores@gmail.com>
2023-12-03 00:00:18 +0000

    Fist commit + sass and settings

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='Home bg-orange-700 font-bold text-5xl p-5'>
+        <h1>Home</h1>
+    </div>
+  )
+}
+
+export default Home;
