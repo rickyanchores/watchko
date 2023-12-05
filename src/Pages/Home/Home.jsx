a49a9383c0422e7530ca70b75b6e3ea84739f910
Ricky Anchores <66031906+rickyanchores@users.noreply.github.com>
2023-12-05 09:36:02 +0000

    insert image and resize

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
+//IMPORTS
+
+
 const Home = () => {
   return (
     <div className='Home font-bold p-5 h-screen'>
-        <div className="Hero-container bg-slate-800 rounded-lg p-5">
-            <img src="" alt="hero" />
+        <div className="Hero-container bg-slate-800 rounded-lg p-5 w-3/6">
+            <img src="https://images.unsplash.com/photo-1508685096489-7aacd43bd3b1?q=80&w=327&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="hero" />
             <h1 className='title text-4xl'>watchko</h1>
             <p>Re-create your own smart watch dial style</p>
             <button className='btn bg-orange-700 p-2 mt-2 rounded-md'>Read more</button>
