f4e482ff593438271ca3f413f02398e65ef93fb0
RickyAnchores <ricky23anchores@gmail.com>
2023-12-05 12:58:33 +0000

    setup grid home

---
+++ src/Components/SocialMedia/SocialMedia.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SocialMedia = () => {
+  return (
+    <div className="SocialMedia bg-slate-900 m-5 rounded-lg p-5 w-2/6 flex items-center justify-center">
+        <h1 className='title text-4xl'>Social Media</h1>
+</div>
+  )
+}
+
+export default SocialMedia;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,21 +1,20 @@
 import React from 'react'
+import SocialMedia from '../../Components/SocialMedia/SocialMedia';
+import Hero from '../../Components/Hero/Hero';
 
 //IMPORTS
 
 
 const Home = () => {
   return (
-    <div className='Home font-bold p-5 flex'>
-        <div className="Hero-container bg-slate-800 rounded-lg p-5 w-4/6">
-            <img src="https://images.unsplash.com/photo-1508685096489-7aacd43bd3b1?q=80&w=327&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="hero" />
-            <h1 className='title text-4xl'>watchko</h1>
-            <p>Re-create your own smart watch dial style</p>
-            <button className='btn bg-orange-700 p-2 mt-2 rounded-md'>Read more</button>
+    <div className='Home font-bold p-5'>
+        <div className="upper p-5 flex">
+          <Hero />
+          <SocialMedia />
         </div>
-        <div className="Social-container bg-slate-900 ml-5 rounded-lg p-5 w-2/6">
-            <h1 className='title text-4xl'>Social Media</h1>
-            <p>Re-create your own smart watch dial style</p>
-            <button className='btn bg-orange-700 p-2 mt-2 rounded-md'>Read more</button>
+        <div className="upper p-5 flex">
+          <SocialMedia />
+          <Hero />
         </div>
     </div>
   )
